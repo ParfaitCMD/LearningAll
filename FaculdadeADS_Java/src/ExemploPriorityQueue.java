85e974d2cf069e5a6e2ce5a73e3696497c6bfaf6
ParfaitCMD <devparfaitdev20@gmail.com>
2025-09-18 22:01:42 -0300

    Update: Atividades orientada a objeto

---
+++ FaculdadeADS_Java/src/ExemploPriorityQueue.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+
+public class ExemploPriorityQueue {
+public static void main(String[] args) {
+
+    PriorityQueue<Integer> filaDeTarefas = new PriorityQueue<>();
+filaDeTarefas.add(3);
+filaDeTarefas.add(1);
+filaDeTarefas.add(2);
+
+System.out.println("Fila de tarefas (ordem interna): " +
+filaDeTarefas);
+System.out.println("Processando tarefa: " +
+filaDeTarefas.poll());
+System.out.println("Processando tarefa: " +
+filaDeTarefas.poll());
+System.out.println("Processando tarefa: " +
+filaDeTarefas.poll());
+    }
+}
